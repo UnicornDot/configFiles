e2ad12d9f01896756037d9897c2cfb1a1d65db58
ancion <ancion@github.com>
2026-01-07 16:31:03 +0800

    update yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,15 +1,25 @@
--- require("yaziline"):setup {
--- 	separator_style = "curvy",
--- 	select_symbol = "",
--- 	yank_symbol = "󰆐",
--- 	filename_max_length = 24, -- trim when filename > 24
--- 	filename_trim_length = 6 -- trim 6 chars from both ends
--- }
+require("yaziline"):setup {
+	separator_style = "curvy",
+	select_symbol = "",
+	yank_symbol = "󰆐",
+	filename_max_length = 24, -- trim when filename > 24
+	filename_trim_length = 6 -- trim 6 chars from both ends
+}
 require("starship"):setup {
 	config_file = "~/.config/yazi/starship.toml",
 }
-require("git"):setup {}
 
+-- 在状态栏中显示符号链接
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+-- 在状态栏中显示用户/文件组
 Status:children_add(function()
 	local h = cx.active.current.hovered
 	if h == nil or ya.target_family() ~= "unix" then
@@ -24,6 +34,14 @@ Status:children_add(function()
 	}
 end, 500, Status.RIGHT)
 
+-- 显示文件的git状态
+require("git"):setup {}
+
 require("yamb"):setup {
 	cli = "fzf",
+}
+
+-- 智能进入目录或打开文件
+require("smart-enter"):setup {
+	open_multi = true,
 }
